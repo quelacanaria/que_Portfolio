0c1b84b64c52c7c00f7665afdd47cf5c37bccb3f
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-11-26 09:09:16 +0800

    commit

---
+++ my-PortFolioV2/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+function Header(){
+
+    return(
+        <>
+            <header>
+                <main>
+                    <section>
+                        <a href=""></a>
+                    </section>
+                    <section>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                    </section>
+                </main>
+            </header>
+        </>
+    )
+
+}
+
+export default Header

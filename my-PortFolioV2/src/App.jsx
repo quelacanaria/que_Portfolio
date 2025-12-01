c042707e8f03db8eed9e5662d73c6347659ee4de
Quekeneth S. Lacanaria <lacanariaquekeneth46@gmail.com>
2025-12-01 19:18:41 +0800

    commit

--- my-PortFolioV2/src/App.jsx
+++ my-PortFolioV2/src/App.jsx
@@ -1,40 +1,22 @@
-import { useRef } from "react";
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Projects from "./Components/Projects";
-import Forms from "./Components/Forms";
-import Card from "./Components/Card";
+import Header from './Components/Header' 
+import Home from './Components/Home' 
+import About from './Components/About' 
+import Projects from './Components/Projects' 
+import Forms from './Components/Forms' 
+import Card from './Components/Card' 
 
-function App() {
-  const refs = {
-    Home: useRef(null),
-    About: useRef(null),
-    Projects: useRef(null),
-    Contact: useRef(null),
-  };
-
-  return (
-    <>
-      <Header sectionRefs={refs} />
-
-      <div id="Home" ref={refs.Home}>
-        <Home />
-      </div>
-
-      <div id="About" ref={refs.About}>
-        <About />
-      </div>
-
-      <div id="Projects" ref={refs.Projects}>
-        <Projects />
-      </div>
-
-      <div id="Contact" ref={refs.Contact}>
-        <Forms />
-      </div>
-    </>
-  );
-}
-
-export default App;
+function App() { 
+  
+    return ( 
+      <> 
+        <Header/> 
+        <Home/> 
+        <Card/> 
+        <About/> 
+        <Projects /> 
+        <Forms/> 
+      </> 
+    ) 
+  } 
+  
+  export default App
--- my-PortFolioV2/src/Components/Forms.jsx
+++ my-PortFolioV2/src/Components/Forms.jsx
@@ -1,18 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function Forms(){
+    const url = 'http://localhost:5000/api/post'
+    const [inputs, setInputs] = useState({
+        name: '',
+        subject: '',
+        email: '',
+        phone: '',
+        message: ''
+    });
+const handlerSubmit = async(event) => {
+    event.preventDefault();
+    try{
+        const response = await axios.post(url, inputs)
+        console.log(response.data)
+
+    }catch(error){
+        console.log('error -> ', error.message);
+
+    }finally{
+        event.target.reset();
+    }       
+    }
+    
+
     return(
         <>
             <div className="w-full h-fit bg-green-xx content-center">
-                <div className="w-[80%] min-w-[320px] max-w-[800px] h-[700px] md:h-[480px] bg-fink-99 mx-auto rounded-2xl content-center">
-                    <form className="w-[80%] min-w-[300px] h-[85%] bg-white mx-auto rounded-2xl grid p-[20px] grid grid-cols-6 grid-rows-8 2xl:grid-rows-8 xl:grid-rows-8 lg:grid-rows-8 md:grid-rows-8 sm:grid-rows-11 gap-x-0 md:gap-x-[10px] gap-y-[5px] hover:scale-105 duration-300 shadow-gg">
+                <div className="w-[80%] min-w-[320px] max-w-[800px] h-[900px] md:h-[560px] bg-fink-99 mx-auto rounded-2xl content-center">
+                    <form onSubmit={handlerSubmit} className="w-[80%] min-w-[300px] h-[85%] bg-white mx-auto rounded-2xl grid p-[20px] grid grid-cols-6  2xl:grid-rows-9 xl:grid-rows-9 lg:grid-rows-9 md:grid-rows-9 sm:grid-rows-12 xs:grid-rows-12 gap-x-0 md:gap-x-[10px] gap-y-[5px] hover:scale-105 duration-300 shadow-gg">
                         <p className="col-span-6 row-span-1 text-center self-center text-[35px] "><i className="fa-solid fa-phone"></i> Contact Me</p>
                         <label className="text-[1.2rem] self-center justify-self-start md:justify-self-end row-span-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-2 xs:col-span-6" htmlFor="">Company Name:</label>
-                        <input className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1" placeholder="CompanyName.Inc" type="text" required/>
+                        <input name='name' onChange={event => setInputs({...inputs, name: event.target.value})} className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1" placeholder="CompanyName.Inc" type="text" required/>
+                        <label className="text-[1.2rem] self-center justify-self-start md:justify-self-end row-span-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-2 xs:col-span-6" htmlFor="">Subject:</label>
+                        <input name='name' onChange={event => setInputs({...inputs, subject: event.target.value})} className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1" placeholder="Job Offer" type="text" required/>
                         <label className="text-[1.2rem] self-center justify-self-start md:justify-self-end row-span-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-2 xs:col-span-6" htmlFor="">Email:</label>
-                        <input className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1" placeholder="johndoe@123.com" type="email" required/>
+                        <input name='email' onChange={event => setInputs({...inputs, email: event.target.value})} className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1" placeholder="johndoe@123.com" type="email" required/>
                         <label className="text-[1.2rem] self-center justify-self-start md:justify-self-end row-span-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-2 xs:col-span-6" htmlFor="">Contact Number:</label>
-                        <input className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1 [&::-webkit-inner-spin-button]:hidden" placeholder="09123456789" inputMode="numeric" type="number" required/>
+                        <input name='phone' onChange={event => setInputs({...inputs, phone: event.target.value})} className="text-[1.2rem] h-[45px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-1 [&::-webkit-inner-spin-button]:hidden" placeholder="09123456789" inputMode="numeric" type="number" required/>
                         <label className="text-[1.2rem] self-center justify-self-start md:justify-self-end row-span-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2 md:col-span-2 xs:col-span-6" htmlFor="">Message:</label>
-                        <textarea className="text-[1.2rem] h-[80px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-2" placeholder="Hi Quekeneth nice meeting you!" type="text" required/>
+                        <textarea name='' onChange={event => setInputs({...inputs, message: event.target.value})} className="text-[1.2rem] h-[80px] p-[5px] border-2 col-span-8 md:col-span-3 row-span-2" placeholder="Hi Quekeneth nice meeting you!" type="text" required/>
                         <button className="w-[90%] md:w-[270px] h-[70px] text-[1.2rem] justify-self-center self-center border-2 bg-fink-99 
                         text-white border-transparent shadow-mine hover:bg-white hover:text-fink-99 hover:border-fink-99
                          hover:shadow-fink-99 hover:scale-120 active:scale-90 active:shadow-none duration-300 col-span-6 row-span-2 ">Send</button>
